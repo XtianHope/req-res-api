ab7f6dc22d847d011abcca914bb4092397a7aa83
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-02-13 22:01:32 -0500

    updated login.handlebars & userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,17 +3,25 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
 
-// Route to get all users
-router.get('/users', async (req, res) => {
+// Route to creeate new users
+router.post('/', async (req, res) => {
   try {
-    const users = await User.findAll();
-    res.json(users);
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(user);
+    });
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server Error'});
+    console.log(err);
+    res.status(500).json(err);
   }
 });
-
 
 router.post('/login', async (req, res) => {
     try {
